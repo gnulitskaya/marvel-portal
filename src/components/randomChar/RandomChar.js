be042383af405f00339d633e905ffaa03fe88622
gnulitskaya <webgnule@mail.ru>
2023-04-11 13:54:03 +0300

    fix error for unique id

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -25,13 +25,17 @@ class RandomChar extends Component {
         this.setState({char, loading: false});
     }
 
+    onCharLoading = (chr) => {
+        this.setState({loading: true});
+    }
+
     onError = () => {
         this.setState({loading: false, error: true});
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(res => {
